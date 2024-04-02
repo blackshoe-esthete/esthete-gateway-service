d6abe390c3eba817ebcae60554f0df8915449373
Subin <70842920+bin-pro@users.noreply.github.com>
2024-04-02 17:51:12 +0900

    Feat: add authorization filter

--- src/main/java/com/blackshoe/esthete/EstheteGatewayServiceApplication.java
+++ src/main/java/com/blackshoe/esthete/EstheteGatewayServiceApplication.java
@@ -2,9 +2,18 @@ package com.blackshoe.esthete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EstheteGatewayServiceApplication {
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstheteGatewayServiceApplication.class, args);
